74746b8045bff4e5c56f2586ef2e7d1da76fce59
Ivan Jimenez <ivan.drt.01.03@gmail.com>
2019-05-01 12:49:53 -0600

    file selector added to NewSocialService component

--- client/src/components/NewSocialService.vue
+++ client/src/components/NewSocialService.vue
@@ -30,6 +30,23 @@
               label="Estado"
             ></v-overflow-btn>
             <br>
+
+          <div class="upload">
+            <slot><input type="file"/></slot>
+            <v-btn small flat color="info" class="custom-upload" @click="showUpload">Agregar Archivos</v-btn>
+            <small :class="{ invalid: !invalid }">
+              {{ files.length }} / {{ max }} Archivos Seleccionados.
+            </small>
+            <ul class="files">
+              <li v-for="(file, index) in files" :key="file">
+                <v-btn small flat color="error" @click="removeFile(index)">
+                  x
+                </v-btn>
+                {{ file.name }}
+              </li>
+            </ul>
+          </div>
+
             <!-- <div
               class="error"
               v-html="error"/>
@@ -46,6 +63,55 @@
 
 <script>
 export default {
+  props: {
+    max: {
+      type: Number,
+      default: 5
+    },
+    value: Array
+  },
+  methods: {
+    /**
+     * Executed, when the user selects a (or multiple) new file(s)
+     * @return {void}
+     */
+    onFileSelection () {
+      // add all selected files
+      for (let file of this.input.files) {
+        this.files.push({ name: file.name })
+      }
+
+      // reset file input
+      this.input.value = null
+    },
+    /**
+     * Removes the file with the given index
+     * @param {number} index
+     * @returns {void}
+     */
+    removeFile (index) {
+      this.files.splice(index)
+    },
+    showUpload () {
+      const event = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true
+      })
+      console.log(event)
+      this.input.dispatchEvent(event)
+    }
+  },
+  computed: {
+    valid () {
+      return this.files.length <= this.max
+    }
+  },
+  watch: {
+    files (files) {
+      this.$emit('input', files)
+    }
+  },
   data () {
     return {
       dropdownCareer: [
@@ -69,8 +135,39 @@ export default {
         programName: '',
         status: '',
         error: null
-      }
+      },
+      // Files stuff
+      files: [],
+      input: null
+    }
+  },
+  mounted () {
+    // Find input
+    this.input = this.$el.querySelector('input[type=file]')
+    this.input.addEventListener('change', () => this.onFileSelection())
+    this.input.style.display = 'none'
+
+    // Select multiple attribute on input, if max is more than one.
+    if (this.max > 1) {
+      this.input.setAttribute('multiple', 'multiple')
+    } else {
+      this.input.removeAttribute('multiple')
+    }
+
+    // Populate internal value, if external value is given,
+    // attempt to populate external value by firing event if not
+    if (this.value) {
+      this.files = this.value
+    } else {
+      this.$emit('input', [])
     }
   }
 }
 </script>
+
+<style scoped>
+.files {
+  list-style: none;
+  text-align: left;
+}
+</style>
